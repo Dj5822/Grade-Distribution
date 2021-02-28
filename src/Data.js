fa23d0feae140394f017bd78981a1991ae24d8e3
dj5822 <dj5822@gmail.com>
2021-03-01 10:58:58 +1300

    Initial commit

---
+++ src/Data.js
@@ -0,0 +1,22 @@
+module.exports = [
+    {
+        subject: "Managing Projects & Innovation",
+        distribution: ["Quizzes - 20%", "Project - 80%"]
+    },
+    
+    {
+        subject: "Heuristics",
+        distribution: ["Peer review - 5%", "Tutorials - 10%", "Test - 25%", "Assignments - 60%"]
+    },
+
+    {
+        subject: "Operating Systems",
+        distribution: ["Quiz - 10%", "Assignment (x3) - 30%", "Exam - 60%"]
+    },
+
+    {
+        subject: "Databases",
+        distribution: ["Lab (x10) - 10%", "Test - 10%", "Assignments (x4) - 20%", "Exam - 60%"]
+    }
+];
+
