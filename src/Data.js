5c25627717196d5de9e2734754eacde5e6fb6e8a
dj5822 <dj5822@gmail.com>
2021-03-02 18:25:16 +1300

    Updated data

--- src/Data.js
+++ src/Data.js
@@ -1,23 +1,39 @@
 var data = [
     {
-        subject: "Managing Projects & Innovation",
-        distribution: ["Quizzes - 20%", "Project - 80%"]
-    },
-    
-    {
-        subject: "Heuristics",
-        distribution: ["Peer review - 5%", "Tutorials - 10%", "Test - 25%", "Assignments - 60%"]
-    },
-
-    {
         subject: "Operating Systems",
-        distribution: ["Quiz - 10%", "Assignment (x3) - 30%", "Exam - 60%"]
+        distribution: ["Assignment 1 - 10% (March 26th)",
+        "Quiz - 10% (March 31st)", 
+        "Assignment 2 - 10% (April 30th)",
+        "Assignment 3 - 10% (May 28th)",
+        "Exam - 60% (June)"]
     },
 
     {
         subject: "Databases",
-        distribution: ["Lab (x10) - 10%", "Test - 10%", "Assignments (x4) - 20%", "Exam - 60%"]
-    }
+        distribution: ["Lab (x10) - 10% (?)", 
+        "Test - 10% (?)", 
+        "Assignments (x4) - 20% (?)", 
+        "Exam - 60% (June)"]
+    },
+    
+    {
+        subject: "Heuristics",
+        distribution: [ "Tutorials (x10) - 10% (March 12, 19, ...)", 
+        "Test - 25% (March 31st)", 
+        "Assignment 1 - 30% (May 10)",
+        "Peer review 1 - 2.5% (May 14)",
+        "Assignment 2 - 30% (May 31st)",
+        "Peer review 2 - 2.5% (June 4th)"]
+    },
+
+    {
+        subject: "Managing Projects & Innovation",
+        distribution: ["Quiz 1 - 5% (March 16th)",
+        "Quiz 2 - 5% (March 29th)",
+        "Quiz 3 - 5% (May 3rd)",
+        "Quiz 4 - 5% (May 25th)", 
+        "Project - 80% (Weekly)"]
+    },
 ];
 
 export default data;
